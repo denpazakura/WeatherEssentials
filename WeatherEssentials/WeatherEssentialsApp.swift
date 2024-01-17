67eb2112553af555c79e5c712fb4d6ee6a1a46c6
Diana Komolova <denpazakura@gmail.com>
2024-01-17 20:33:39 +0100

    Project created

---
+++ WeatherEssentials/WeatherEssentialsApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherEssentialsApp.swift
+//  WeatherEssentials
+//
+//  Created by Diana Komolova on 17/01/2024.
+//
+
+import SwiftUI
+
+@main
+struct WeatherEssentialsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
